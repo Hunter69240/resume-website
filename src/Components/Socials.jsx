6cfacf4406909f0e42181cb5a11a6d3a3c1ed303
Hunter69240 <aadishds@gmail.com>
2025-05-15 22:14:19 +0530

    Responsive-8-15/5/25

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -2,7 +2,7 @@ import { FaInstagram, FaLinkedin, FaGithub, FaEnvelope} from 'react-icons/fa';
 function Socials (){
     return (
         <>
-        <div className="flex flex-col w-fit  fixed top-1/4 gap-4 text mx-4">
+        <div className="flex flex-col w-fit  fixed top-1/4 gap-4 text">
             <FaEnvelope className='hover:text-blue-600'/> 
             <FaInstagram className='hover:text-blue-600'/>
             <FaLinkedin className='hover:text-blue-600'/>
