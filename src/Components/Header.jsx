affff9b1154074c14debb08a31880b2de312746c
Hunter69240 <aadishds@gmail.com>
2025-05-21 23:22:43 +0530

    Download-21-05025

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,15 +11,14 @@ function Header() {
                 </div>
 
                 <div>
-                    <a
-                      href="/Aadish_CV .pdf"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="mx-3 my-3 bg-[#8B5CF6] text-white rounded-full w-25 p-2 shadow-lg hover:bg-red-500"
+                    <a 
+                        href="/Aadish_CV .pdf" 
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="mx-3 my-3 bg-[#8B5CF6] text-white rounded-full w-25 p-2 shadow-lg hover:bg-red-500"
                     >
-                    View Resume
+                        Download
                     </a>
-
                 </div>
             </div>
         </>
