b8e3490de4b25498443ca262b7c870e93ee63f46
Hunter69240 <aadishds@gmail.com>
2025-05-25 22:18:12 +0530

    CV-Update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
 
                 <div>
                     <a 
-                        href="/Aadish_CV .pdf" 
+                        href="/Aadish_CV.pdf" 
                         target="_blank"
                         rel="noopener noreferrer"
                         className="mx-3 my-3 bg-[#8B5CF6] text-white rounded-full w-25 p-2 shadow-lg hover:bg-red-500"
