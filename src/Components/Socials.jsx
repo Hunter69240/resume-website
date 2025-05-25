4f15d138c6936b00780a2d23acdab0061cfbabd3
Hunter69240 <aadishds@gmail.com>
2025-05-25 21:23:07 +0530

    Socials-Added

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -3,10 +3,13 @@ function Socials (){
     return (
         <>
         <div className="flex flex-col w-fit  fixed top-1/4 gap-4 text-xl sm:text-2xl">
-            <FaEnvelope className='hover:text-blue-600 text-white'/> 
-            <FaInstagram className='hover:text-blue-600 text-red-400'/>
-            <FaLinkedin className='hover:text-blue-600 text-blue-500'/>
-            <FaGithub className='hover:text-blue-600 text-black'/>
+
+          <a href="mailto:aadishds@gmail.com">
+                    <FaEnvelope className="hover:text-blue-600 text-white" />
+                </a> 
+          <a href='https://www.instagram.com/mr_hunter69/'>  <FaInstagram className='hover:text-blue-600 text-red-400'/></a>
+          <a href='https://www.linkedin.com/in/aadish-d-somayaji-787b952a8/'> <FaLinkedin className='hover:text-blue-600 text-blue-500'/> </a> 
+          <a href='https://github.com/Hunter69240'>  <FaGithub className='hover:text-blue-600 text-black'/></a>
             
         </div>
         </>
