5ee9c82415d75e8d019fc782ceb3010524c307a3
Hunter69240 <aadishds@gmail.com>
2026-03-13 11:11:09 +0530

    Videos compressed

--- src/Components/ProjectsSection/Projects.tsx
+++ src/Components/ProjectsSection/Projects.tsx
@@ -4,37 +4,37 @@ function Projects(){
     const projects=[
         {
             name: "AquaScope",
-            vid: "videos/AquaScope.mkv",
+            vid: "videos/Aquascope.mp4",
             desc: "AquaScope is an interactive 3D fish encyclopedia that allows users to discover detailed biological data, habitat info, and immersive 3D models of fish species worldwide. With intelligent search and a clean ocean-themed UI, it provides an engaging way to explore aquatic life.",
             git: "https://github.com/Hunter69240/Majorpp-Frontend"
         },
         {
             name: "GDHelper",
-            vid: "videos/GDHelper.mp4",
+            vid: "videos/Gdhelper.mp4",
             desc: "GDHelper is a Dockerized full-stack AI speech evaluation app that transcribes responses, analyzes fluency and reasoning using embeddings, and generates structured LLM feedback.",
             git: "https://github.com/Hunter69240/GDHelper"
         },
         {
             name: "MyPokedex",
-            vid: "videos/MyPokedex.mp4", 
+            vid: "videos/Mypokedex.mp4", 
             desc:"MyPokedex is a sleek React Native app that lets users explore Pokémon with detailed info, images, and search functionality. It uses PokéAPI and responsive UI to deliver a fast, engaging experience on mobile.",
             git:"https://github.com/Hunter69240/MyPokedex"
         },
         {
             name: "ShopFinder",
-            vid: "videos/ShopFinder.mp4", 
+            vid: "videos/Shopfinder.mp4", 
             desc:"ShopFinder is a location-based React Native app that helps users quickly discover nearby shops using GPS. With features like category filters, Google Maps integration, and favorite shop management, it offers a smooth and practical experience for everyday needs.",
             git:"https://github.com/Hunter69240/ShopFinder"
         },
         {
             name: "WeatherApp",
-            vid: "videos/WeatherApp.mp4", 
+            vid: "videos/Weatherapp.mp4", 
             desc:"WeatherApp is a clean React Native application that displays real-time weather information for any location. It fetches data using the OpenWeatherMap API and features a user-friendly interface with search functionality and location-based updates.",
             git:"https://github.com/Hunter69240/WeatherApp"
         },
         {
             name: "JsonGlance",
-            vid: "videos/JsonGlance.mkv", 
+            vid: "videos/Jsonglance.mp4", 
             desc: "JsonGlance is a web-based utility that allows users to paste JSON data and instantly visualize its hierarchical structure. Built with React, it offers an intuitive interface for inspecting and navigating complex JSON objects, making it ideal for developers, API users, and learners working with JSON data.",
             git: "https://github.com/Hunter69240/JsonGlance"
         },
@@ -47,13 +47,13 @@ function Projects(){
         },
         {
             name: "FishKnower",
-            vid: "videos/FishKnower.mp4", 
+            vid: "videos/Fishknower.mp4", 
             desc:"FishKnower is an interactive React Native app that lets users explore and learn about different fish species through a searchable interface. With dynamic habitat-based backgrounds, Appwrite-powered data, and embedded 3D model viewing, it delivers an immersive underwater discovery experience for marine enthusiasts and students alike.",
             git:"https://github.com/Hunter69240/FishKnower"
         },
         {
             name: "MyNotes",
-            vid: "videos/MyNotes.mp4", 
+            vid: "videos/Mynotes.mp4", 
             desc: "MyNotes is a full-stack note-taking app built with React Native and Express.js. It allows users to create, edit, and delete notes with a clean, responsive UI. The backend uses MongoDB for storage and exposes RESTful APIs, while the frontend offers seamless integration and real-time updates.",
             git: "https://github.com/Hunter69240/mynotes"
         },
@@ -68,7 +68,7 @@ function Projects(){
 
         {
             name: "DFA Visualizer",
-            vid: "videos/DFAVisualizerDemo.mkv", 
+            vid: "videos/Dfavisualizer.mp4", 
             desc: "DFA Visualizer is a Python-based simulator that lets users define custom DFAs, validate input strings, and generate animated GIFs showing each transition step. Built using NetworkX, Matplotlib, and PIL, it’s perfect for visually understanding automata behavior.",
             git: "https://github.com/Hunter69240/DFAVisualizer"
         },
